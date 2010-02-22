6281f20493e886afb67f30e3290c96e8d7c6a9a6
foo bar <argriffi@ubusrv20100110a.statgen.ncsu.edu>
2010-02-22 00:21:33 -0500

    add some pypi stuff

---
+++ hmmus/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        # required
+        name = 'hmmus',
+        version = '0.0.7',
+        author = 'Alex Griffing',
+        author_email = 'argriffi@ncsu.edu',
+        url = 'http://github.com/argriffing/hmmus',
+        # recommended
+        packages = ['hmmus'],
+        description = 'Hidden Markov model stuff')
