0e30b773e89d4f1ba20635d7c119b3836521aa95
foo bar <argriffi@ubusrv20100110a.statgen.ncsu.edu>
2010-02-24 11:24:57 -0500

    view-matrix.py now requires only the number of columns to be specified

--- view-matrix.py
+++ view-matrix.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 
-import sys
-
 import argparse
 import numpy as np
 
 def main(args):
-    shape = (args.nrows, args.ncols)
-    M = np.fromfile(args.infile, dtype=float).reshape(shape)
+    """
+    Print a view of the matrix.
+    """
+    M = np.fromfile(args.infile, dtype=float)
+    nrows = len(M) / args.ncols
+    shape = (nrows, args.ncols)
+    M = M.reshape(shape)
     print M
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('nrows', type=int, help='number of rows'),
     parser.add_argument('ncols', type=int, help='number of columns'),
-    parser.add_argument('infile',
-            type=argparse.FileType('rb'), nargs='?', default=sys.stdin,
+    parser.add_argument('infile', type=argparse.FileType('rb'),
             help='binary file to view')
     main(parser.parse_args())
 
