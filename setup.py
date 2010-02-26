03a8dcc3a463b7e2089501bf9c661fdeda30bf00
foo bar <argriffi@ubusrv20100110a.statgen.ncsu.edu>
2010-02-26 13:53:01 -0500

    version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 from distutils.core import Extension
 import os
 
-myversion_tuple = (0, 1, 21)
+myversion_tuple = (0, 1, 22)
 myversion = '.'.join(str(x) for x in myversion_tuple)
 
 hummusc = Extension(
