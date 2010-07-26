20136eab0f3ea01b8a3e63199dec35c4af91023e
alex griffing <argriffi@ncsu.edu>
2010-07-25 20:26:19 -0400

    more progress on the hmm-in-ram implementation

--- hmmusnodiskmodule.c
+++ hmmusnodiskmodule.c
@@ -16,9 +16,20 @@ struct baum {
   Py_buffer emiss; /* small input emission matrix */
   Py_buffer trans_expect; /* small output transition expectation matrix */
   Py_buffer emiss_expect; /* small output emission expectation matrix */
-  int nstates; /* # hidden states; -1 if unknown; -2 if error */
-  int nalpha; /* # unique emission symbols; -1 if unknown; -2 if error */
-  int nobs; /* # observations; -1 if unknown; -2 if error */
+  int nstates; /* # hidden states or -1 if unknown */
+  int nalpha; /* # unique emission symbols or -1 if unknown */
+  int nobs; /* # observations or -1 if unknown */
+  PyObject *v_obj;
+  PyObject *l_obj;
+  PyObject *f_obj;
+  PyObject *s_obj;
+  PyObject *b_obj;
+  PyObject *d_obj;
+  PyObject *distn_obj;
+  PyObject *trans_obj;
+  PyObject *emiss_obj;
+  PyObject *trans_expect_obj;
+  PyObject *emiss_expect_obj;
 };
 
 int baum_init(struct baum *p)
@@ -37,6 +48,39 @@ int baum_init(struct baum *p)
   p->nstates = -1;
   p->nalpha = -1;
   p->nobs = -1;
+  p->v_obj = NULL;
+  p->l_obj = NULL;
+  p->f_obj = NULL;
+  p->s_obj = NULL;
+  p->b_obj = NULL;
+  p->d_obj = NULL;
+  p->distn_obj = NULL;
+  p->trans_obj = NULL;
+  p->emiss_obj = NULL;
+  p->trans_expect_obj = NULL;
+  p->emiss_expect_obj = NULL;
+}
+
+int safe_buffer_release(Py_buffer *pbuf)
+{
+  if (pbuf->buf) PyBuffer_Release(pbuf);
+  return 0;
+}
+
+int baum_destroy(struct baum *p)
+{
+  safe_buffer_release(&p->v);
+  safe_buffer_release(&p->l);
+  safe_buffer_release(&p->f);
+  safe_buffer_release(&p->s);
+  safe_buffer_release(&p->b);
+  safe_buffer_release(&p->d);
+  safe_buffer_release(&p->distn);
+  safe_buffer_release(&p->trans);
+  safe_buffer_release(&p->emiss);
+  safe_buffer_release(&p->trans_expect);
+  safe_buffer_release(&p->emiss_expect);
+  return 0;
 }
 
 int baum_set_nobs(struct baum *p, int nobs)
@@ -179,152 +223,147 @@ int baum_set_f(struct baum *p, PyObject *pobj)
   return 0;
 }
 
-int baum_get_nstates(struct baum *p, int *pnstates)
+int baum_set_s(struct baum *p, PyObject *pobj)
 {
-  int retval = baum_get_nstates(p);
-  if (retval == -1) {
-    PyErr_SetString(HmmusnodiskError,
-        "The number of hidden states could not be determined.");
-    return -1;
-  } else if (retval == -2) {
-    PyErr_SetString(HmmusnodiskError,
-        "The input arrays have incompatible numbers of hidden states.");
-    return -1;
-  } else {
-    *pnstates = retval;
-    return 0;
-  }
+  char name[] = "The scaling matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->s) < 0) return -1;
+  if (check_ndim(&p->s, 1, name) < 0) return -1;
+  if (check_datatype_double(&p->s, name) < 0) return -1;
+  if (baum_set_nobs(p, p->s.shape[0]) < 0) return -1;
+  return 0;
 }
 
-
-int get_emission_buffer()
+int baum_set_b(struct baum *p, PyObject *pobj)
 {
-  /* assert that the objects support the buffer interface */
-  PyObject *pyobjects[] = {emission_obj};
-  char *names[] = {"the emission matrix"};
-  if (check_interfaces(1, pyobjects, names) < 0) {
-    except = 1; goto end;
-  }
-  /* get the buffer view for each object */
-  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
-  if (PyObject_GetBuffer(emission_obj, &emission_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_emission_buffer = 1;
-  }
-  /* check the buffer shapes */
-  if (emission_buffer.ndim != 2)
-  {
-    PyErr_SetString(HmmusbufError,
-        "the emission matrix should be two dimensional");
-    except = 1; goto end;
-  }
+  char name[] = "The backward matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->b) < 0) return -1;
+  if (check_ndim(&p->b, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->b, name) < 0) return -1;
+  if (baum_set_nobs(p, p->b.shape[0]) < 0) return -1;
+  if (baum_set_nstates(p, p->b.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_d(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The posterior matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->d) < 0) return -1;
+  if (check_ndim(&p->d, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->d, name) < 0) return -1;
+  if (baum_set_nobs(p, p->d.shape[0]) < 0) return -1;
+  if (baum_set_nstates(p, p->d.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_distn(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The stationary distribution matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->distn) < 0) return -1;
+  if (check_ndim(&p->distn, 1, name) < 0) return -1;
+  if (check_datatype_double(&p->distn, name) < 0) return -1;
+  if (baum_set_nstates(p, p->distn.shape[0]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_trans(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The transition matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->trans) < 0) return -1;
+  if (check_ndim(&p->trans, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->trans, name) < 0) return -1;
+  if (baum_set_nstates(p, p->trans.shape[0]) < 0) return -1;
+  if (baum_set_nstates(p, p->trans.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_emiss(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The emission probability matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->emiss) < 0) return -1;
+  if (check_ndim(&p->emiss, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->emiss, name) < 0) return -1;
+  if (baum_set_nstates(p, p->emiss.shape[0]) < 0) return -1;
+  if (baum_set_nalpha(p, p->emiss.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_trans_expect(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The transition expectation matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->trans_expect) < 0) return -1;
+  if (check_ndim(&p->trans_expect, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->trans_expect, name) < 0) return -1;
+  if (baum_set_nstates(p, p->trans_expect.shape[0]) < 0) return -1;
+  if (baum_set_nstates(p, p->trans_expect.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_set_emiss_expect(struct baum *p, PyObject *pobj)
+{
+  char name[] = "The emission expectation matrix";
+  if (check_buffer_interface(pobj, name) < 0) return -1;
+  if (get_buffer(pobj, &p->emiss_expect) < 0) return -1;
+  if (check_ndim(&p->emiss_expect, 2, name) < 0) return -1;
+  if (check_datatype_double(&p->emiss_expect, name) < 0) return -1;
+  if (baum_set_nstates(p, p->emiss_expect.shape[0]) < 0) return -1;
+  if (baum_set_nalpha(p, p->emiss_expect.shape[1]) < 0) return -1;
+  return 0;
+}
+
+int baum_read_buffers(struct baum *p)
+{
+  if (p->v_obj != NULL)
+    if (baum_set_v(p, p->v_obj) < 0) return -1;
+  if (p->l_obj != NULL)
+    if (baum_set_l(p, p->l_obj) < 0) return -1;
+  if (p->f_obj != NULL)
+    if (baum_set_f(p, p->f_obj) < 0) return -1;
+  if (p->s_obj != NULL)
+    if (baum_set_s(p, p->s_obj) < 0) return -1;
+  if (p->b_obj != NULL)
+    if (baum_set_b(p, p->b_obj) < 0) return -1;
+  if (p->d_obj != NULL)
+    if (baum_set_d(p, p->d_obj) < 0) return -1;
+  if (p->distn_obj != NULL)
+    if (baum_set_distn(p, p->distn_obj) < 0) return -1;
+  if (p->trans_obj != NULL)
+    if (baum_set_trans(p, p->trans_obj) < 0) return -1;
+  if (p->emiss_obj != NULL)
+    if (baum_set_emiss(p, p->emiss_obj) < 0) return -1;
+  if (p->trans_expect_obj != NULL)
+    if (baum_set_trans_expect(p, p->trans_expect_obj) < 0) return -1;
+  if (p->emiss_expect_obj != NULL)
+    if (baum_set_emiss_expect(p, p->emiss_expect_obj) < 0) return -1;
+  return 0;
 }
 
 static PyObject *
 finite_alphabet_likelihoods_python(PyObject *self, PyObject *args)
 {
   int except = 0;
-  /* init buffer info */
-  int got_emission_buffer = 0;
-  Py_buffer emission_buffer;
-  /* read the args */
-  PyObject *emission_obj;
-  const char *v_name;
-  const char *l_name;
-  if (!PyArg_ParseTuple(args, "Oss",
-        &emission_obj, &v_name, &l_name)) {
+  struct baum bm;
+  baum_init(&bm);
+  if (!PyArg_ParseTuple(args, "OOO",
+        &bm.emiss_obj, &bm.v_obj, &bm.l_obj)) {
     except = 1; goto end;
   }
-  /* assert that the objects support the buffer interface */
-  PyObject *pyobjects[] = {emission_obj};
-  char *names[] = {"the emission matrix"};
-  if (check_interfaces(1, pyobjects, names) < 0) {
+  if (baum_read_buffers(&bm) < 0) {
     except = 1; goto end;
   }
-  /* get the buffer view for each object */
-  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
-  if (PyObject_GetBuffer(emission_obj, &emission_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_emission_buffer = 1;
-  }
-  /* check the buffer shapes */
-  if (emission_buffer.ndim != 2)
-  {
-    PyErr_SetString(HmmusbufError,
-        "the emission matrix should be two dimensional");
-    except = 1; goto end;
-  }
-  /* run the algorithm */
-  int nstates = emission_buffer.shape[0];
-  int nalpha = emission_buffer.shape[1];
-  if (do_finite_alphabet_likelihoods(nstates, nalpha, emission_buffer.buf,
-        v_name, l_name))
-  {
-    PyErr_SetString(HmmusbufError, "finite_alphabet_likelihoods error");
+  if (finite_alphabet_likelihoods_nodisk(bm.nstates, bm.nalpha,
+        bm.emiss.buf, bm.v.buf, bm.l.buf) < 0) {
+    PyErr_SetString(HmmusnodiskError, "finite_alphabet_likelihoods error");
     except = 1; goto end;
   }
 end:
-  /* cleanup */
-  if (got_emission_buffer) {
-    PyBuffer_Release(&emission_buffer);
-  }
-  /* return an appropriate value */
-  if (except) {
-    return NULL;
-  } else {
-    return Py_BuildValue("i", 42);
-  }
-}
-
-static PyObject *
-state_expectations_python(PyObject *self, PyObject *args)
-{
-  int except = 0;
-  /* init buffer info */
-  int got_expectations_buffer = 0;
-  Py_buffer expectations_buffer;
-  /* read the args */
-  PyObject *expectations_obj;
-  const char *d_name;
-  if (!PyArg_ParseTuple(args, "Os",
-        &expectations_obj, &d_name)) {
-    except = 1; goto end;
-  }
-  /* assert that the objects support the buffer interface */
-  PyObject *pyobjects[] = {expectations_obj};
-  char *names[] = {"the expectations vector"};
-  if (check_interfaces(1, pyobjects, names) < 0) {
-    except = 1; goto end;
-  }
-  /* get the buffer view for each object */
-  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
-  if (PyObject_GetBuffer(expectations_obj, &expectations_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_expectations_buffer = 1;
-  }
-  /* check the buffer shapes */
-  if (expectations_buffer.ndim != 1)
-  {
-    PyErr_SetString(HmmusbufError,
-        "the expectations vector should be one dimensional");
-    except = 1; goto end;
-  }
-  /* run the algorithm */
-  int nstates = expectations_buffer.shape[0];
-  if (do_state_expectations(nstates, expectations_buffer.buf, d_name))
-  {
-    PyErr_SetString(HmmusbufError, "state_expectations error");
-    except = 1; goto end;
-  }
-end:
-  /* cleanup */
-  if (got_expectations_buffer) {
-    PyBuffer_Release(&expectations_buffer);
-  }
-  /* return an appropriate value */
+  bm_destroy(&bm);
   if (except) {
     return NULL;
   } else {
@@ -336,87 +375,24 @@ static PyObject *
 transition_expectations_python(PyObject *self, PyObject *args)
 {
   int except = 0;
-  /* init buffer info */
-  int got_expectations_buffer = 0;
-  int got_trans_buffer = 0;
-  Py_buffer expectations_buffer;
-  Py_buffer trans_buffer;
-  /* read the args */
-  PyObject *expectations_obj;
-  PyObject *trans_obj;
-  const char *l_name;
-  const char *f_name;
-  const char *b_name;
-  if (!PyArg_ParseTuple(args, "OOsss",
-        &trans_obj, &expectations_obj, 
-        &l_name, &f_name, &b_name)) {
+  struct baum bm;
+  baum_init(&bm);
+  if (!PyArg_ParseTuple(args, "OOOOO",
+        &bm.trans_obj, &bm.trans_expect_obj,
+        &bm.l_obj, &bm.f_obj, &bm.b_obj)) {
     except = 1; goto end;
   }
-  /* assert that the objects support the buffer interface */
-  PyObject *pyobjects[] = {trans_obj, expectations_obj};
-  char *names[] = {"the transition matrix", "the expectations matrix"};
-  if (check_interfaces(2, pyobjects, names) < 0) {
+  if (baum_read_buffers(&bm) < 0) {
     except = 1; goto end;
   }
-  /* get the buffer view for each object */
-  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
-  if (PyObject_GetBuffer(trans_obj, &trans_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_trans_buffer = 1;
-  }
-  if (PyObject_GetBuffer(expectations_obj, &expectations_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_expectations_buffer = 1;
-  }
-  /* check the buffer shapes */
-  if (trans_buffer.ndim != 2) {
-    PyErr_SetString(HmmusbufError,
-        "the transition matrix should be two dimensional");
-    except = 1; goto end;
-  }
-  if (expectations_buffer.ndim != 2) {
-    PyErr_SetString(HmmusbufError,
-        "the expectations matrix should be two dimensional");
-    except = 1; goto end;
-  }
-  if (trans_buffer.shape[0] != trans_buffer.shape[1])
-  {
-    PyErr_SetString(HmmusbufError,
-        "the transition matrix should be square");
-    except = 1; goto end;
-  }
-  if (expectations_buffer.shape[0] != expectations_buffer.shape[1])
-  {
-    PyErr_SetString(HmmusbufError,
-        "the expectations matrix should be square");
-    except = 1; goto end;
-  }
-  if (trans_buffer.shape[0] != expectations_buffer.shape[0])
-  {
-    PyErr_SetString(HmmusbufError,
-        "the transition and expectations matrices should be the same size");
-    except = 1; goto end;
-  }
-  /* run the algorithm */
-  int nstates = expectations_buffer.shape[0];
-  if (do_transition_expectations(nstates,
-        trans_buffer.buf, expectations_buffer.buf,
-        l_name, f_name, b_name))
-  {
+  if (transition_expectations_nodisk(bm.nstates,
+        bm.trans.buf, bm.trans_expect.buf,
+        bm.l.buf, bm.f.buf, bm.b.buf)) {
     PyErr_SetString(HmmusbufError, "transition_expectations error");
     except = 1; goto end;
   }
 end:
-  /* cleanup */
-  if (got_trans_buffer) {
-    PyBuffer_Release(&trans_buffer);
-  }
-  if (got_expectations_buffer) {
-    PyBuffer_Release(&expectations_buffer);
-  }
-  /* return an appropriate value */
+  baum_destroy(&bm);
   if (except) {
     return NULL;
   } else {
@@ -428,52 +404,22 @@ static PyObject *
 emission_expectations_python(PyObject *self, PyObject *args)
 {
   int except = 0;
-  /* init buffer info */
-  int got_expectations_buffer = 0;
-  Py_buffer expectations_buffer;
-  /* read the args */
-  PyObject *expectations_obj;
-  const char *v_name;
-  const char *d_name;
-  if (!PyArg_ParseTuple(args, "Oss",
-        &expectations_obj, &v_name, &d_name)) {
+  struct baum bm;
+  baum_init(&bm);
+  if (!PyArg_ParseTuple(args, "OOO",
+        &bm.emiss_expect_obj, &bm.v_obj, &bm.d_obj)) {
     except = 1; goto end;
   }
-  /* assert that the objects support the buffer interface */
-  PyObject *pyobjects[] = {expectations_obj};
-  char *names[] = {"the expectations matrix"};
-  if (check_interfaces(1, pyobjects, names) < 0) {
+  if (baum_read_buffers(&bm) < 0) {
     except = 1; goto end;
   }
-  /* get the buffer view for each object */
-  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
-  if (PyObject_GetBuffer(expectations_obj, &expectations_buffer, flags) < 0) {
-    except = 1; goto end;
-  } else {
-    got_expectations_buffer = 1;
-  }
-  /* check the buffer shapes */
-  if (expectations_buffer.ndim != 2)
-  {
-    PyErr_SetString(HmmusbufError,
-        "the expectations matrix should be two dimensional");
-    except = 1; goto end;
-  }
-  /* run the algorithm */
-  int nstates = expectations_buffer.shape[0];
-  int nalpha = expectations_buffer.shape[1];
-  if (do_emission_expectations(nstates, nalpha, expectations_buffer.buf,
-        v_name, d_name))
-  {
+  if (emission_expectations_nodisk(bm.nstates, bm.nalpha,
+        bm.emiss_expect.buf, bm.v.buf, bm.d.buf)) {
     PyErr_SetString(HmmusbufError, "emissions_expectations error");
     except = 1; goto end;
   }
 end:
-  /* cleanup */
-  if (got_expectations_buffer) {
-    PyBuffer_Release(&expectations_buffer);
-  }
-  /* return an appropriate value */
+  baum_destroy(&bm);
   if (except) {
     return NULL;
   } else {
@@ -486,102 +432,26 @@ sequence_log_likelihood_python(PyObject *self, PyObject *args)
 {
   int except = 0;
   double log_likelihood = 0.0;
-  /* read the args */
-  const char *s_name;
-  if (!PyArg_ParseTuple(args, "s", &s_name))
-  {
+  struct baum bm;
+  baum_init(&bm);
+  if (!PyArg_ParseTuple(args, "O", &bm.s_obj)) {
     except = 1; goto end;
   }
-  /* get the log likelihood */
-  if (do_sequence_log_likelihood(&log_likelihood, s_name))
-  {
+  if (baum_read_buffers(&bm) < 0) {
+    except = 1; goto end;
+  }
+  if (sequence_log_likelihood_nodisk(&log_likelihood, bm.s.buf) < 0) {
     PyErr_SetString(HmmusbufError, "sequence_log_likelihood error");
     except = 1; goto end;
   }
 end:
+  baum_destroy(&bm);
   if (except) {
     return NULL;
   } else {
     return Py_BuildValue("d", log_likelihood);
   }
 }
-
-static PyObject *
-fwdbwd_somedisk_python(PyObject *self, PyObject *args)
-{
-  int except = 0;
-  /* init an object */
-  struct HMBUFA hmbufa;
-  HMBUFA_clear(&hmbufa);
-  /* read the args */
-  PyObject *distn;
-  PyObject *trans;
-  const char *l_name;
-  const char *d_name;
-  if (!PyArg_ParseTuple(args, "OOss",
-      &distn, &trans,
-      &l_name, &d_name)) {
-    except = 1; goto end;
-  }
-  /* do extensive error checking */
-  if (HMBUFA_init(&hmbufa, distn, trans) < 0) {
-    except = 1; goto end;
-  }
-  /* run the hmm algorithm */
-  if (do_fwdbwd_somedisk(&hmbufa.tm, l_name, d_name))
-  {
-    PyErr_SetString(HmmusbufError, "fwdbwd_somedisk algorithm error");
-    except = 1; goto end;
-  }
-end:
-  HMBUFA_destroy(&hmbufa);
-  if (except) {
-    return NULL;
-  } else {
-    return Py_BuildValue("i", 42);
-  }
-}
-
-/* Try to use the buffer interface.
- * Python args are the numpy arrays:
- * distribution_in
- * transitions_in
- * likelihoods_in
- * posterior_out
- */
-static PyObject *
-fwdbwd_nodisk_python(PyObject *self, PyObject *args)
-{
-  int except = 0;
-  /* init an object */
-  struct HMBUFB hmbufb;
-  HMBUFB_clear(&hmbufb);
-  /* read the args */
-  PyObject *distn, *trans, *like, *post;
-  if (!PyArg_ParseTuple(args, "OOOO", &distn, &trans, &like, &post)) {
-    except = 1; goto end;
-  }
-  /* do extensive error checking */
-  if (HMBUFB_init(&hmbufb, distn, trans, like, post) < 0) {
-    except = 1; goto end;
-  }
-  /* fill the posterior matrix */
-  if (fwdbwd_nodisk(&hmbufb.hmbufa.tm, hmbufb.like.shape[0],
-      (double *) hmbufb.like.buf, (double *) hmbufb.post.buf) < 0) {
-    PyErr_SetString(HmmusbufError,
-        "failed to run the forward-backward algorithm");
-    except = 1; goto end;
-  }
-end:
-  HMBUFB_destroy(&hmbufb);
-  /* return an appropriate value */
-  if (except) {
-    return NULL;
-  } else {
-    return Py_BuildValue("i", 42);
-  }
-}
-
 
 static PyMethodDef HmmusnodiskMethods[] = {
   {"finite_alphabet_likelihoods",
