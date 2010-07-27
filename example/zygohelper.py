84f3471526102db3335f4a06dbe8a3cae1f4eea4
alex griffing <argriffi@ncsu.edu>
2010-07-27 18:56:21 -0400

    add a two-state example HMM and add a progress bar option to the example app

--- example/zygohelper.py
+++ example/zygohelper.py
@@ -11,14 +11,7 @@ from scipy import optimize
 
 from hmmus import hmm
 from hmmus import estimation
-
-# state 0: homozygous
-# state 1: heterozygous
-# state 2: bad
-
-# emission 0: ACGT
-# emission 1: MRWSYK
-# emission 2: N
+from hmmus import progress
 
 def gen_nonempty_stripped(raw_lines):
     for line in raw_lines:
@@ -42,7 +35,7 @@ def fasta_to_raw_observations(raw_lines):
 
 class Summary:
 
-    def __init__(self, model, show_ticks=False):
+    def __init__(self, model, pbar=None, show_ticks=False):
         """
         @param model: something like an estimation.FiniteModel object
         """
@@ -50,6 +43,7 @@ class Summary:
         self.out = StringIO()
         self.iteration = 0
         self.log_likelihoods = []
+        self.pbar = pbar
         self.show_ticks = show_ticks
 
     def finish(self):
@@ -92,51 +86,65 @@ class Summary:
         print >> self.out
         print >> self.out
         self.log_likelihoods.append(self.model.get_log_likelihood())
+        if self.pbar:
+            self.pbar.increment()
         if self.show_ticks:
             sys.stderr.write('.')
         self.iteration += 1
 
 
 def main(args, letter_to_emission, trans, emiss):
+    # loading + initial state + iterations
+    if args.pbar:
+        pbar = progress.Bar(1 + 1 + args.n)
+    else:
+        pbar = None
     with open(args.fasta) as fin:
         raw_observations = fasta_to_raw_observations(fin.readlines())
         arr = [letter_to_emission[c] for c in raw_observations]
         observations = np.array(arr, dtype=np.int8)
+    if pbar:
+        pbar.increment()
     model = estimation.FiniteModel(trans, emiss, observations)
-    summary = Summary(model)
+    summary = Summary(model, pbar, args.ticks)
     estimation.baum_welch(
         model.update, model.get_expectations, trans, emiss, args.n,
         callback=summary.after_baum_welch)
     summary_text = summary.finish()
+    suffix = args.common_suffix
     if args.summary:
-        with open(args.summary, 'w') as fout:
+        with open(args.summary + suffix, 'w') as fout:
             print >> fout, summary_text
     if args.log_likelihoods:
-        with open(args.log_likelihoods, 'w') as fout:
+        with open(args.log_likelihoods + suffix, 'w') as fout:
             print >> fout, '\n'.join('%f' % x for x in summary.log_likelihoods)
     if args.posterior:
         hmm.pretty_print_posterior(raw_observations,
-                model.get_posterior(), 60, args.posterior)
+                model.get_posterior(), 60, args.posterior + suffix)
     if args.posterior_decoding:
         hmm.pretty_print_posterior_decoding(raw_observations,
-                model.get_posterior(), 60, args.posterior_decoding)
+                model.get_posterior(), 60, args.posterior_decoding + suffix)
 
 def run(letter_to_emission, trans, emiss):
     parser = argparse.ArgumentParser()
     parser.add_argument('--fasta', required=True,
             help='read this single sequence fasta file')
     parser.add_argument('--ticks', action='store_true',
-            help='draw a mark for each baum welch iteration')
+            help='draw a mark for each search iteration')
+    parser.add_argument('--pbar', action='store_true',
+            help='show a progress bar')
     parser.add_argument('--n', default=20, type=int,
             help='do this many total search iterations')
     #parser.add_argument('--method', choices=['bw', 'nm'], default='bw',
     #help='pure Baum-Welch vs. Nelder-Mead-supplemented search')
-    parser.add_argument('--posterior',
-            help='write a probabilistic posterior decoding to this file')
     parser.add_argument('--posterior_decoding',
             help='write a hard posterior decoding to this file')
+    parser.add_argument('--posterior',
+            help='write a probabilistic posterior decoding to this file')
     parser.add_argument('--log_likelihoods',
             help='write a list of sequence log likelihoods to this file')
     parser.add_argument('--summary',
             help='write a summary of baum welch iterations to this file')
+    parser.add_argument('--common_suffix', default='',
+            help='this suffix is appended to output file names')
     main(parser.parse_args(), letter_to_emission, trans, emiss)
---
+++ example/zygosity2.py
@@ -0,0 +1,31 @@
+"""
+Analyze a fasta file.
+"""
+
+import numpy as np
+
+import zygohelper
+
+# state 0: homozygous and missing
+# state 1: heterozygous
+
+# emission 0: ACGTN
+# emission 1: MRWSYK
+
+g_letter_to_emission = {
+        'A':0, 'C':0, 'G':0, 'T':0, 'N':0,
+        'M':1, 'R':1, 'W':1, 'S':1, 'Y':1, 'K':1}
+
+g_default_trans = np.array([
+    [0.9, 0.1],
+    [0.1, 0.9]])
+
+g_default_emiss = np.array([
+    [0.9, 0.1],
+    [0.5, 0.5]])
+
+if __name__ == '__main__':
+    zygohelper.run(
+            g_letter_to_emission,
+            g_default_trans,
+            g_default_emiss)
