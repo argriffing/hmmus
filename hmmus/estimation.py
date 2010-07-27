586c68a30cb962cbb58f1fbc7246e121ec021a29
alex griffing <argriffi@ncsu.edu>
2010-07-27 12:07:40 -0400

    translate between a list of constrained matrices and a list of parameters which define the matrices

--- hmmus/estimation.py
+++ hmmus/estimation.py
@@ -6,14 +6,15 @@ An example of the estimation of HMM parameters is Baum-Welch.
 Baum-Welch is an EM algorithm for HMMs,
 but black box optimization is also possible.
 Matrices and vectors are numpy arrays.
-One exception is the tuple of serialized parameters
-for black box optimization.
 Another restriction for this estimation framework is that we
 assume that we see only a single sequence,
 and that the first element of the sequence is chosen
 according to the stationary distribution of the model,
 and that that sequence is ended at an arbitrary point.
 """
+
+import math
+import unittest
 
 import numpy as np
 
@@ -45,6 +46,75 @@ def get_stationary_distribution(T):
     best_v = np.array([abs(x) for x in best_v])
     # Force the elements of the dominant eigenvector to sum to one.
     return best_v / best_v.sum()
+
+def irregular_grouper(seq, sizes):
+    """
+    Yield irregularly sized chunks of a sequence.
+    @param seq: a sequence
+    @param sizes: an iterable of sizes
+    """
+    i = 0
+    for k in sizes:
+        yield seq[i:i+k]
+        i += k
+
+def _row_mask_to_nparams(mask):
+    """
+    @param mask: a boolean array
+    """
+    nparams = mask.sum() - 1
+    if nparams < 0:
+        msg = 'a stochastic vector cannot be all zeros'
+        raise ValueError(msg)
+    return nparams
+
+def _serialize_row(row, mask):
+    """
+    This is for stochastic vectors where some elements are forced to zero.
+    Such a vector is defined by a number of parameters equal to
+    the length of the vector minus one and minus the number of elements
+    forced to zero.
+    @param row: an array of floats
+    @param mask: row-conformant bools such that False forces zero probability
+    @return: a shorter array of transformed floats
+    """
+    nparams = _row_mask_to_nparams(mask)
+    if not nparams:
+        return []
+    ref_value = row[mask][0]
+    params = np.log(row[mask][1:] / ref_value)
+    return params
+
+def _deserialize_row(params, mask):
+    """
+    This is for stochastic vectors where some elements are forced to zero.
+    Such a vector is defined by a number of parameters equal to
+    the length of the vector minus one and minus the number of elements
+    forced to zero.
+    @param params: an array of statistical parameters
+    @param mask: bools such that False forces zero probability
+    @return: a mask-conformant list of nonnegative floats
+    """
+    row = np.zeros(mask.shape)
+    row[mask] = [1.0] + np.exp(params).tolist()
+    row /= row.sum()
+    return row
+
+def _matrix_mask_to_nparams(mask):
+    """
+    Input is a mask for a constrained stochastic vector.
+    @param mask: a boolean array
+    """
+    return sum(_row_mask_to_nparams(row) for row in mask)
+
+def _serialize_matrix(M, mask):
+    return np.hstack([_serialize_row(r, m) for r, m in zip(M, mask)])
+
+def _deserialize_matrix(params, mask):
+    sizes = [_row_mask_to_nparams(row) for row in mask]
+    ps = list(irregular_grouper(params, sizes))
+    return np.vstack([_deserialize_row(p, r) for p, r in zip(ps, mask)])
+
 
 class FiniteModel:
     """
@@ -93,8 +163,40 @@ class FiniteModel:
         self.distn = None
         self.trans_expect = None
         self.emiss_expect = None
+        # mark the locations of nonzeros in original matrices
+        self.trans_mask = (self.trans != 0)
+        self.emiss_mask = (self.emiss != 0)
         # define the evaluation level
         self.eval_level = EVAL_NONE
+
+    def update(self, trans, emiss):
+        self.trans = trans
+        self.emiss = emiss
+        self.eval_level = EVAL_NONE
+
+    def serialize_params(self):
+        """
+        @return: a float array
+        """
+        return np.hstack([
+            _serialize_matrix(self.trans, self.trans_mask),
+            _serialize_matrix(self.emiss, self.emiss_mask)])
+
+    def deserialize_params(self, params):
+        """
+        @return: a transition matrix and an emission matrix
+        """
+        ntrans_params = _matrix_mask_to_nparams(self.trans_mask)
+        nemiss_params = _matrix_mask_to_nparams(self.emiss_mask)
+        if len(params) != ntrans_params + nemiss_params:
+            msg_a = 'got %d parameters ' + len(params)
+            msg_b = 'but expected %d' % ntrans_params + nemiss_params
+            raise ValueError(msg_a + msg_b)
+        trans_params = params[:ntrans_params]
+        emiss_params = params[-nemiss_params:]
+        trans = _deserialize_matrix(trans_params, self.trans_mask)
+        emiss = _deserialize_matrix(emiss_params, self.emiss_mask)
+        return trans, emiss
 
     def _eval_stationary(self):
         self.distn = get_stationary_distribution(self.trans)
@@ -132,8 +234,42 @@ class FiniteModel:
                 self.emiss_expect, self.v_big, self.d_big)
         self.eval_level = max(self.eval_level, EVAL_ALL)
 
-    def update(self, trans, emiss):
-        self.trans = trans
-        self.emiss = emiss
-        self.eval_level = EVAL_NONE
+    def get_log_likelihood(self):
+        if self.eval_level < EVAL_LOG_LIKELIHOOD:
+            self._eval_log_likelihood()
+        return self.log_likelihood
+    
+
+class TestEstimation(unittest.TestCase):
+
+    def setUp(self):
+        obs = np.array([0,1,2], dtype=np.int8)
+        trans = np.array([
+            [0.4, 0.3, 0.3],
+            [0.3, 0.4, 0.3],
+            [0.3, 0.3, 0.4]])
+        emiss = np.array([
+            [0.8, 0.1, 0.1],
+            [0.1, 0.8, 0.1],
+            [0.0, 0.0, 1.0]])
+        self.m = FiniteModel(trans, emiss, obs)
+
+    def test_parameter_serialization_consistency(self):
+        params = self.m.serialize_params()
+        trans, emiss = self.m.deserialize_params(params)
+        self.assertTrue(np.allclose(trans, self.m.trans))
+        self.assertTrue(np.allclose(emiss, self.m.emiss))
+
+    def test_parameter_serialization(self):
+        e_params = np.array([
+                -0.28768207, -0.28768207,
+                0.28768207, 0.,
+                0., 0.28768207,
+                -2.07944154, -2.07944154,
+                2.07944154, 0.])
+        o_params = self.m.serialize_params()
+        self.assertTrue(np.allclose(e_params, o_params))
+
+if __name__ == '__main__':
+    unittest.main()
 
