c91696735eed420bbf13b3cb177d3a652efaff69
alex griffing <argriffi@ncsu.edu>
2010-07-28 23:16:25 -0400

    add a parallel example app

--- hmmus/zygohelper.py
+++ hmmus/zygohelper.py
@@ -4,6 +4,8 @@ Analyze a fasta file.
 
 from StringIO import StringIO
 import sys
+import multiprocessing
+import os
 
 import argparse
 import numpy as np
@@ -147,3 +149,64 @@ def run(letter_to_emission, trans, emiss, description=None):
             help='this suffix is appended to output file names')
     main(parser.parse_args(), letter_to_emission,
             np.array(trans), np.array(emiss))
+
+def parallel_worker(params):
+    """
+    @param params: a bunch of parameter packed into a sequence
+    """
+    # unpack the params
+    user_params, hmm_params = params
+    f_in, d_out, niterations = user_params
+    letter_to_emission, trans, emiss = hmm_params
+    trans = np.array(trans)
+    emiss = np.array(emiss)
+    # define output path names
+    suffix = '.' + os.path.basename(f_in) + ('.n%d.txt' % niterations)
+    summary_name = os.path.join(d_out, 'summary' + suffix)
+    log_likelihoods_name = os.path.join(d_out, 'log.likelihood' + suffix)
+    posterior_name = os.path.join(d_out, 'soft.decoding' + suffix)
+    posterior_decoding_name = os.path.join(d_out, 'hard.decoding' + suffix)
+    # read the fasta file
+    with open(f_in) as fin:
+        raw_observations = fasta_to_raw_observations(fin.readlines())
+        arr = [letter_to_emission[c] for c in raw_observations]
+        observations = np.array(arr, dtype=np.int8)
+    # do the baum welch stuff
+    model = estimation.FiniteModel(trans, emiss, observations)
+    summary = Summary(model, None, None)
+    estimation.baum_welch(
+        model.update, model.get_expectations, trans, emiss, niterations,
+        callback=summary.after_baum_welch)
+    summary_text = summary.finish()
+    # write the output files
+    with open(summary_name, 'wt') as fout:
+        print >> fout, summary_text
+    with open(log_likelihoods_name, 'wt') as fout:
+        print >> fout, '\n'.join('%f' % x for x in summary.log_likelihoods)
+    hmm.pretty_print_posterior(raw_observations,
+            model.get_posterior(), 60, posterior_name)
+    hmm.pretty_print_posterior_decoding(raw_observations,
+            model.get_posterior(), 60, posterior_decoding_name)
+
+def is_fasta(filename):
+    return filename.endswith('.fa') or filename.endswith('.fasta')
+
+def main_parallel(args, letter_to_emission, trans, emiss):
+    filenames_in = [x for x in os.listdir(args.input_dir) if is_fasta(x)]
+    pathnames_in = [os.path.join(args.input_dir, x) for x in filenames_in]
+    pool = multiprocessing.Pool(args.nprocesses)
+    user_params = [[f_in, args.output_dir, args.n] for f_in in pathnames_in]
+    hmm_params = [[letter_to_emission, trans, emiss] for x in pathnames_in]
+    pool.map(parallel_worker, zip(user_params, hmm_params))
+
+def run_parallel(letter_to_emission, trans, emiss, description=None):
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--input_dir', required=True,
+            help='segment all .fa and .fasta files in this directory')
+    parser.add_argument('--output_dir', required=True,
+            help='put the segmentations in this directory')
+    parser.add_argument('--nprocesses', default=8, type=int,
+            help='use this many processes in parallel')
+    parser.add_argument('--n', default=200, type=int,
+            help='do this many baum welch iterations per file')
+    main_parallel(parser.parse_args(), letter_to_emission, trans, emiss)
